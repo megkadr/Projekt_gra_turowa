41ed0f91d4005b47d2acfef2f87ead2c162089c3
Megakadr <minizgrywus@gmail.com>
2022-06-29 17:46:45 +0200

    Code cleanup and refactoring

--- Projekt/Unit.cpp
+++ Projekt/Unit.cpp
@@ -1,87 +1,97 @@
 #include <iostream>
 #include "Unit.h"
 
-using namespace std;
+void player::attack(Unit* target)
+{
+	target->HP -= dmg;
+	std::cout << "Atakujesz potwora za:" << " " << dmg << std::endl << std::endl;
+	if (target->HP <= 0)
+	{
+		std::cout << "potwor nie zyje" << std::endl;
+	}
+}
 
-void player::atak(Unit* target)
+int player::is_dead() const
 {
-    target-> HP -= dmg;
-    cout << "Atakujesz potwora za:" << " " << dmg << endl << endl;
-    if (target->HP <= 0) {
-        cout << "potwor nie zyje" << endl;
-    }
+	if (player::HP > 0)
+	{
+		return 1;
+	}
+	else
+		return 0;
 }
-int player::is_dead()
-{
-    if (player::HP > 0) {
-        return 1;
-    }
-    else
-        return 0;
-}
+
 void player::heal()
 {
-    cout << "leczysz sie za:" << " " << dmg << endl;
-    HP = HP + dmg;
+	std::cout << "leczysz sie za:" << " " << dmg << std::endl;
+	HP = HP + dmg;
 }
+
 void player::show_stats()
 {
-    cout << "Staty gracza:" << endl;
-    cout << "HP:" << " " << HP << endl;
-    cout << "Dmg:" << " " << dmg << endl<<endl;
+	std::cout << "Staty gracza:" << std::endl;
+	std::cout << "HP:" << " " << HP << std::endl;
+	std::cout << "Dmg:" << " " << dmg << std::endl << std::endl;
 }
-void monster::atak(Unit* target)
+
+void monster::attack(Unit* target)
 {
-    target->HP -= dmg;
-    cout << "Potwor atakuje za:" << " " << dmg << endl << endl;
-    if (target->HP <= 0) {
-        cout << "nie zyjesz" << endl;
-    }
+	target->HP -= dmg;
+	std::cout << "Potwor atakuje za:" << " " << dmg << std::endl << std::endl;
+	if (target->HP <= 0)
+	{
+		std::cout << "nie zyjesz" << std::endl;
+	}
 }
+
 int monster::is_dead()
 {
-    if (monster::HP > 0) {
-        return 1;
-    }
-    else
-        return 0;
+	if (monster::HP > 0)
+	{
+		return 1;
+	}
+	else
+		return 0;
 }
+
 void monster::show_stats()
 {
-    cout << "Staty potwora:" << endl;
-    cout << "HP:" << " " << HP << endl;
-    cout << "Dmg:" << " " << dmg << endl<<endl;
+	std::cout << "Staty potwora:" << std::endl;
+	std::cout << "HP:" << " " << HP << std::endl;
+	std::cout << "Dmg:" << " " << dmg << std::endl << std::endl;
 }
+
 player::player(int hp, int d)
 {
-    HP = hp;
-    dmg = d;
+	HP = hp;
+	dmg = d;
 }
+
 monster::monster(int hp1, int d1)
 {
-    HP = hp1;
-    dmg = d1;
+	HP = hp1;
+	dmg = d1;
 }
 
 trap::trap(int d2)
 {
-    dmg = d2;
+	dmg = d2;
 }
 
 void trap::obr(Unit* target)
 {
-    target->HP -= dmg;
-    cout << "Otrzymujesz obrazenia od pulapki w wysokosci:" << " " << dmg << endl << endl;
+	target->HP -= dmg;
+	std::cout << "Otrzymujesz obrazenia od pulapki w wysokosci:" << " " << dmg << std::endl << std::endl;
 }
 
 chest::chest(int buff2, int zdr2)
 {
-    buff = buff2;
-    zdr = zdr2;
+	buff = buff2;
+	zdr = zdr2;
 }
 
 void chest::obr(Unit* target)
 {
-    target->HP += zdr; 
-    cout << "Otrzymujesz z skrzyni leczenie w wysokosci:" << " " << zdr << endl << endl;
+	target->HP += zdr;
+	std::cout << "Otrzymujesz z skrzyni leczenie w wysokosci:" << " " << zdr << std::endl << std::endl;
 }
--- Projekt/Unit.h
+++ Projekt/Unit.h
@@ -1,49 +1,64 @@
 #include <iostream>
-using namespace std;
 
-class Unit {
+class Unit
+{
 public:
-    int HP;
-    int dmg;
-    virtual void atak(Unit* target) = 0;
+	int HP;
+	int dmg;
+	virtual void attack(Unit* target) = 0;
 };
-class player :public Unit {
-public:
-    player(int = 100, int = 10);
-    int is_dead();
-    void heal();
-    virtual void atak(Unit* target) override;
-    virtual void show_stats();
-};
-class monster :public Unit {
-public:
-    monster(int = 50, int = 5);
-    int is_dead();
-    virtual void atak(Unit* target) override;
-    virtual void show_stats();
-};
-class zombie :public monster {
 
-};
-class chest {
+class player : public Unit
+{
 public:
-    chest(int = 15, int = 10);
-    int buff;
-    int zdr;
-    virtual void obr(Unit* target);
+	player(int = 100, int = 10);
+	int is_dead() const;
+	void heal();
+	virtual void attack(Unit* target) override;
+	virtual void show_stats();
 };
-class trap {
+
+class monster : public Unit
+{
 public:
-    trap(int = 15);
-    int dmg;
-    virtual void obr(Unit* target);
+	monster(int = 50, int = 5);
+	int is_dead();
+	virtual void attack(Unit* target) override;
+	virtual void show_stats();
 };
-class trap_chest :public chest {
-    int dmg;
+
+class zombie : public monster
+{
 };
-class dart_trap :public trap {
-    int dmg;
+
+class chest
+{
+public:
+	chest(int = 15, int = 10);
+	int buff;
+	int zdr;
+	virtual void obr(Unit* target);
 };
-class mud_trap :public trap {
-    int dmg;
+
+class trap
+{
+public:
+	trap(int = 15);
+	int dmg;
+	virtual void obr(Unit* target);
+};
+
+class trap_chest : public chest
+{
+	int dmg;
+};
+
+class dart_trap : public trap
+{
+	int dmg;
+};
+
+class mud_trap : public trap
+{
+	int dmg;
 };
